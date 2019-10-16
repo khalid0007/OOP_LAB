1979a4cf92dac8ddbe735a3a815973151d5ebabd
Khalid Hassan Sk <52673145+khalid0007@users.noreply.github.com>
2019-10-16 16:32:53 +0530

    Update as4_6.cpp

--- as4_6.cpp
+++ as4_6.cpp
@@ -1,145 +1,220 @@
 #include<iostream>
-#include<string.h>
+#include<strings.h>
+
+#define __MAX_SIZE__ 20
+
 using namespace std;
-class subject;
-class details;
-class student
-{
-	char name[20];
-	int roll,no[10];
-	public:
-		student(char* s,int r,int *a)
-		{
-			strcpy(name,s);
-			roll=r;
-			for(int i=0;i<10;i++)
-			no[i]=a[i];
-		}
-		
-		friend class details;
-		friend class subject;
-		
+
+class student_list;
+class subject_list;
+class relation;
+
+class student{
+private:
+	int roll;
+	char name[30];
+public:
+	student(int r, char* n)
+	{
+		roll = r;
+		strcpy(name, n);
+	}
+
+	student(const student& a)
+	{
+		roll = a.roll;
+		strcpy(name, a.name);
+	}
+
+	friend class student_list;
 };
 
 class subject
 {
-	char sub_name[15];
+public:
 	int code;
-	public:
-		subject(char*s ,int c)
-		{
-			strcpy(sub_name,s);
-			code=c;
-		}
-		
-		friend class details;
-		friend class student;
-		
+	char name[30];
+public:
+	subject(int c, char* n)
+	{
+		code = c;
+		strcpy(name, n);
+	}
+
+	subject(const subject& s)
+	{
+		code = s.code;
+		strcpy(name, s.name);
+	}
+
+	friend class subject_list;
 };
 
-class details
-{
-	int students_for_subject[5][5];
-	int subjects_for_student[5][5];
-	public:
-		details()
-		{
-			for(int i=0;i<5;i++)
-			{
-				for(int j=0;j<5;j++)
-				{
-					students_for_subject[i][j]=-1;
-					subjects_for_student[i][j]=-1;
-				}
-			}
-		}
-		void update(int* a,int x,int sz)
-		{
-			for(int i=0;i<sz;i++)
-			{
-				for(int j=0;j<5;j++)
-				{
-			
-				if(subjects_for_student[x][j]==-1)
-				{
-				subjects_for_student[x][j]=a[i];
-				break;
-				}
-				
-				}
-				
-				for(int j=0;j<5;j++)
-				{
-					if(students_for_subject[a[i]][j]==-1)
-					{
-					students_for_subject[a[i]][j]=x;
-					break;
-					}
-					
-				}
-			}
-			
-			
-		}
-		void update2(int sub,int* a,int sz)
-		{
-				
-				for(int i=0;i<sz;i++){
-				
-				for(int j=0;j<5;j++)
-				{
-					if(students_for_subject[sub][j]==-1)
-					{
-					students_for_subject[sub][j]=a[i];
-					break;
-					}
-					
-				}
-				
-				
-				
-				for(int j=0;j<5;j++)
-				{
-			
-				if(subjects_for_student[a[i]][j]==-1)
-				{
-				subjects_for_student[a[i]][j]=sub;
-				break;
-				}
-				
-				}
-			}
-		}
-		void display()
-		{
-			for(int i=0;i<5;i++)
-			{
-				cout<<"subjects for student with roll "<<i+1<<" ";
-				for(int j=0;j<5;j++)
-				{
-					if(subjects_for_student[i][j]==-1)
-					break;
-					
-					cout<<subjects_for_student[i][j]<<" ";
-				}
-				cout<<endl;
-			}
-			
-			for(int i=0;i<5;i++)
-			{
-				cout<<"students for subject with code "<<i+1<<" ";
-				for(int j=0;j<5;j++)
-				{
-					if(students_for_subject[i][j]==-1)
-					break;
-					
-					cout<<students_for_subject[i][j]<<" ";
-				}
-				cout<<endl;
-			}
-		}
+class student_list{
+private:
+	int top;
+	student l[__MAX_SIZE__];
+public:
+	student_list() {top = 0;}
+	void insert(const student& a)
+	{
+		l[top++] = a;
+	}
+
+	void insert(int r, char* n)
+	{
+		l[top].roll = r;
+		strcpy(l[top++].name, n);
+	}
+
+	student& operator[](int indx)
+	{
+		if(indx >= top) {cerr << "Index out of range!!"; return l[top+1];}
+
+		else return l[indx]; 
+	}
+
+	friend class relation;
+	friend ostream& operator<<(ostream& stream, student_list a);
 };
+
+ostream& operator<<(ostream& stream, student_list a)
+{
+	if(a.top == 0) {cout << "No student opted for corresponding subject!!\n"; return stream;}
+
+	for(int i = 0; i < a.top; i++) stream << a[i] << "\n";
+
+	return stream;
+}
+
+
+class subject_list{
+private:
+	int top;
+	subject l[__MAX_SIZE__];
+public:
+	subject_list() {top = 0;}
+	void insert(const subject& a)
+	{
+		l[top++] = a;
+	}
+
+	void insert(int c, char* n)
+	{
+		l[top].code = c;
+		strcpy(l[top++].name, n);
+	}
+
+	subject operator[](int indx)
+	{
+		if(indx >= top) {cerr << "Index out of range!!"; return l[top+1];}
+
+		else return l[indx]; 
+	}
+
+	friend class relation;
+	friend ostream& operator<<(ostream& stream, subject_list a);
+};
+
+ostream& operator<<(ostream& stream, subject_list a)
+{
+	if(a.top == 0) {cout << "No subject opted!!\n"; return stream;}
+
+	for(int i = 0; i < a.top; i++) stream << a[i] << "\n";
+
+	return stream;
+}
+
+
+
+class relation{
+	student_list stdl;
+	subject_list subl;
+	bool relate[__MAX_SIZE__][__MAX_SIZE__];
+
+	int find_student(int roll)
+	{
+		for(int i = 0; i < stdl.top; i++) {
+			if(stdl[i].roll == roll) return i;
+		}
+
+		cerr << "Student don't exist!!\n";
+		return __MAX_SIZE__;
+	}
+
+	int find_subject(int code)
+	{
+		for(int i = 0; i < subl.top; i++)
+		{
+			if(subl[i].code == code) return i;
+		}
+
+		cerr << "Student don't exist!!\n";
+		return __MAX_SIZE__;
+	}
+
+public:
+	relation()
+	{
+		memset(relate, false, sizeof(relate));
+	}
+
+	void add_student(const student& s)
+	{
+		stdl.insert(s);
+	}
+
+	void add_subject(const subject& s)
+	{
+		subl.insert(s);
+	}
+
+	void opt(int std_roll, int sub_code)
+	{
+		int stdindx = find_student(std_roll);
+		int subindx = find_subject(sub_code);
+
+		if(subindx == __MAX_SIZE__) {cerr << "Can't find subject!!\n"; return;}
+		if(stdindx == __MAX_SIZE__) {cerr << "Can't find student!!\n"; return;}
+
+		relate[stdindx][subindx] = true;
+		relate[subindx][stdindx] = true;
+	}
+
+	void show_student(int subcode)
+	{
+		int subindx = find_subject(subcode);
+
+		if(subindx == __MAX_SIZE__) {cerr << "Can't find subject!!\n"; return;}
+
+		student_list temp;
+
+		for(int i = 0; i < stdl.top; i++)
+		{
+			if(relate[subindx][i]) temp.insert(stdl[i]);
+		}
+
+		cout << temp;
+	}
+
+	void show_subject(int stdroll)
+	{
+		int stdindx = find_student(stdroll);
+
+		if(stdindx == __MAX_SIZE__) {cerr << "Can't find student!!\n"; return;}
+
+		subject_list temp;
+
+		for(int i = 0; i < subl.top; i++)
+		{
+			if(relate[stdindx][i]) temp.insert(subl[i]);
+		}
+
+		cout << temp;
+	}
+}
+
 int main()
 {
-	
+	return 0;
 }
